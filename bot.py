6a64eb72b60428d5fcbe04375c5003f473a5ea10
CheesyPotato <cheesypotato@users.noreply.github.com>
2018-07-18 16:25:06 -0400

    Initial commit except again

--- bot.py
+++ bot.py
@@ -10,9 +10,10 @@ bot = commands.Bot(command_prefix='!')
 
 async def checkloop():
     while True:
-        await asyncio.sleep(5)
+
         filevar = open('users.txt')
         filevarread = filevar.readlines()
+        filevarcopy = filevarread.copy()
         for i in filevarread:
             filevarread[filevarread.index(i)] = filevarread[filevarread.index(i)].replace('\n', '')
         filevar.close()
@@ -26,8 +27,9 @@ async def checkloop():
                 oldnick = bot.get_server(i[2]).get_member(i[0]).name.split(' SR: ', 1)[0]
             else:
                 oldnick = bot.get_server(i[2]).get_member(i[0]).nick.split(' SR: ', 1)[0]
-            await bot.change_nickname(bot.get_server(i[2]).get_member(i[0]), oldnick + ' SR: ' + str(r.json()['competitive']['rank']))
 
+            await bot.change_nickname(bot.get_server(i[2]).get_member(i[0]), oldnick + ' SR: ' + str(r.json()['competitive']['rank']))
+        await asyncio.sleep(5)
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -37,15 +39,18 @@ async def on_ready():
     bot.loop.create_task(checkloop())
 
 @bot.command(pass_context=True)
-async def start(ctx, user):
+async def start(ctx, user=None):
+    if user == None:
+        await bot.say('Error: put your battlenet after !start')
+        return
+    if ctx.message.author.id == ctx.message.server.owner.id:
+        await bot.say("Sorry, the bot can't change the owner's nickname :sob:")
+        return
     user = user.replace('#','-')
     r = requests.get('http://ow-api.herokuapp.com/profile/pc/us/' + user)
     owstats = r.json()
     if owstats == {}:
         await bot.say('Error: account not found.')
-        return
-    if r.json()['competitive']['rank'] > 4000:
-        await bot.say('get fuckin baited, i know youre trying to cheat')
         return
 
     f = open('users.txt', 'r')
@@ -53,9 +58,12 @@ async def start(ctx, user):
     f.seek(0)
     filereadlines = f.readlines()
     f.close()
-    for i in filereadlines:
-        if i == ctx.message.author.id:
-            await bot.say('dont say start twice')
+    a = iter(filereadlines)
+    filereadlist = list(zip(a,a,a))
+    print(filereadlist)
+    for i in filereadlist:
+        if i[0].replace('\n', '') == ctx.message.author.id and i[2].replace('\n', '') == ctx.message.server.id:
+            await bot.say('Error: you have already registered in this server.')
             return
     f = open('users.txt', 'w')
     if fileread == '':
@@ -63,6 +71,24 @@ async def start(ctx, user):
     else:
         f.write(fileread + '\n' + ctx.message.author.id + '\n' + user + '\n' + ctx.message.server.id)
     f.close()
-
+    await bot.say('Success! updating...')
+@bot.command(pass_context=True)
+async def remove(ctx):
+    f = open('users.txt', 'r')
+    fileread = f.readlines()
+    f.seek(0)
+    f.close()
+    a = iter(fileread)
+    filereadlist = list(zip(a,a,a))
+    for i in filereadlist:
+        if i[0].replace('\n', '') == ctx.message.author.id and i[2].replace('\n', '') == ctx.message.server.id:
+            filereadlist.remove(i)
+            f = open('users.txt', 'w')
+            f.writelines([i for sub in filereadlist for i in sub])
+            f.close()
+            await bot.say('Removed!')
+            return
+    await bot.say("You haven't registered yet in this server (!start)")
 
 bot.run(token)
+#await bot.say('Error: not enough permissions (can the bot manage nicknames? is the member higher than the bot?)')
